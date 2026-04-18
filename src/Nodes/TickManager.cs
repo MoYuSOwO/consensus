5e7ad9481fa500b455e71390ad6a3fe68e39d300
MoYuSOwO <175416583+MoYuSOwO@users.noreply.github.com>
2026-04-18 14:10:26 +0800

    feat: move to Nodes, add timer and introduce singleton

--- src/Nodes/TickManager.cs
+++ src/Nodes/TickManager.cs
@@ -1,8 +1,14 @@
 using Godot;
 using System;
 
+namespace Consensus.Models;
+
 public partial class TickManager : Node
 {
+	public static TickManager? Instance { get; private set; }
+
+	[Export] public float TickInterval = 0.1f;
+
 	public int CurrentTick { get; private set; } = 0;
     
     [Signal]
@@ -18,10 +24,35 @@ public partial class TickManager : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Instance = this;
+
+		foreach (var node in GetChildren())
+		{
+			node.QueueFree();
+		}
+
+        Timer timer = new()
+        {
+            Name = "TickTimer",
+			WaitTime = TickInterval,
+			OneShot = false,
+			Autostart = true
+        };
+		timer.Timeout += OnTimerTimeout;
+        AddChild(timer);
+
+		timer.Start();
+
+		GD.Print($"[TickManager] Timer starts, interval: {TickInterval}");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public override void _ExitTree()
+	{
+		if (Instance == this) Instance = null;
 	}
 }
